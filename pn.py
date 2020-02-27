b9921d170243f492441ccff7f3885e3a56867d27
Nicolas Amat <nicolas.amat@grenoble-inp.org>
2020-02-27 15:03:11 +0100

    Remove NetParser class

--- pn.py
+++ pn.py
@@ -16,10 +16,11 @@ class PetriNet:
     - a finite set of places
     - a finite set of transitions
     """
-    def __init__(self, id):
-        self.id = id
+    def __init__(self, filename):
+        self.id = ""
         self.places = {}
         self.transitions = {}
+        self.parseNet(filename)
 
     def __str__(self):
         text = "net {}\n".format(self.id)
@@ -28,6 +29,51 @@ class PetriNet:
         for tr in self.transitions.values():
             text += str(tr)
         return text
+
+    def parseNet(self, filename):
+            try:
+                with open(filename, 'r') as fp:
+                    for line in fp.readlines():
+                        content = line.strip().split(' ')
+                        element = content.pop(0)
+                        if element == "net":
+                            self.id = content[0]
+                        if element == "tr":
+                            self.parseTransition(content)
+                        if element == "pl":
+                            self.parsePlace(content)
+                fp.close()
+            except FileNotFoundError as e:
+                exit(e)
+
+    def parseTransition(self, content):
+        tr = Transition(content.pop(0))
+        
+        arrow = content.index("->")
+        src = content[1:arrow]
+        dst = content[arrow + 1:]
+
+        for pl in src:
+            if pl not in self.places:
+                self.places[pl] = Place(pl)
+            tr.src.append(self.places.get(pl))
+
+        for pl in dst:
+            if pl not in self.places:
+                self.places[pl] = Place(pl)
+            tr.dest.append(self.places.get(pl))
+
+        self.transitions[tr.id] = tr
+
+
+    def parsePlace(self, content):
+        placeId = content[0]
+        marking = content[1].replace('(', '').replace(')', '')
+    
+        if placeId not in self.places:
+            self.places[placeId] = Place(placeId, marking)
+        else:
+            self.places.get(placeId).marking = marking
 
 class Place:
     """
@@ -67,63 +113,9 @@ class Transition:
         text += '\n'
         return text
 
-class NetParser:
-    """
-    Petri Net Parser
-    Input: .net file format
-    """
-    def __init__(self, filename):
-        self.net = None
-        self.parseNet(filename)
-
-    def parseNet(self, filename):
-        try:
-            with open(filename, 'r') as fp:
-                for line in fp.readlines():
-                    content = line.strip().split(' ')
-                    element = content.pop(0)
-                    if element == "net":
-                        self.net = PetriNet(content[0])
-                    if element == "tr":
-                        self.parseTransition(content)
-                    if element == "pl":
-                        self.parsePlace(content)
-            fp.close()
-        except FileNotFoundError as e:
-            exit(e)
-
-    def parseTransition(self, content):
-        tr = Transition(content.pop(0))
-        
-        arrow = content.index("->")
-        src = content[1:arrow]
-        dst = content[arrow + 1:]
-
-        for pl in src:
-            if pl not in self.net.places:
-                self.net.places[pl] = Place(pl)
-            tr.src.append(self.net.places.get(pl))
-
-        for pl in dst:
-            if pl not in self.net.places:
-                self.net.places[pl] = Place(pl)
-            tr.dest.append(self.net.places.get(pl))
-
-        self.net.transitions[tr.id] = tr
-
-
-    def parsePlace(self, content):
-        placeId = content[0]
-        marking = content[1].replace('(', '').replace(')', '')
-       
-        if placeId not in self.net.places:
-            self.net.places[placeId] = Place(placeId, marking)
-        else:
-            self.net.places.get(placeId).marking = marking
-
 
 if __name__ == "__main__":
     if (len(sys.argv) == 1):
         exit("File missing: ./np <path_to_file>")
-    parser = NetParser(sys.argv[1])
-    print(parser.net)
+    net = PetriNet(sys.argv[1])
+    print(net)
