4ff74c5cac27046847304642fc2e5857f348b170
Nicolas Amat <nicolas.amat@grenoble-inp.org>
2020-03-02 00:43:03 +0100

    Marking enumeration

---
+++ marking.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+"""
+Marking
+
+Input file format: .aut
+Documentation: http://projects.laas.fr/tina//manuals/formats.html
+"""
+
+from pn import *
+import re
+
+class Marking:
+    def __init__(self, filename, pn):
+        self.pn = pn
+        self.markings = []
+        self.parseMarking(filename)
+
+    def __str__(self):
+        text = ""
+        for marking in self.markings:
+            text += "-> "
+            for place, counter in marking.items():
+                text += "{}:{} ".format(place, counter)
+            text += "\n"
+        return text
+
+    def smtlib(self):
+        text = ""
+        // TODO
+        return text
+
+    def parseMarking(self, filename):
+        try:
+            with open(filename, 'r') as fp:
+                for line in fp.readlines():
+                    content = line.strip().replace('(', '').replace(')', '').split(',')
+                    if content[0] == content[-1]:
+                        places = re.split('\s+', content[1].replace('"', ''))
+                        consistent = True
+                        marking = dict()
+                        for place in places:
+                            place_data = place.split('.')
+                            if place_data[0] != 'S':
+                                consistent = False
+                                break
+                            marking[place_data[1].replace('`', '')] = 1
+                        if consistent:
+                            self.markings.append(marking)
+        except FileNotFoundError as e:
+            exit(e)
+
+
+if __name__=='__main__':
+    if (len(sys.argv) == 1):
+        exit("File missing: ./marking <path_to_file>")
+    net = PetriNet(sys.argv[1])
+    marks = Marking(sys.argv[1], net)
+    print("TODO")
+    print("-------------")
+    print(marks)
+    print("\nSMTlib2 Format")
+    print("--------------")
+    # print(phi.smtlib())
