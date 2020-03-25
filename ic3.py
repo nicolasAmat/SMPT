91eb91bd5044dee10d94c07d549d73b028fc016a
Nicolas Amat <nicolas.amat@grenoble-inp.org>
2020-03-25 23:31:24 +0100

    IC3: minor improvment

--- ic3.py
+++ ic3.py
@@ -53,6 +53,7 @@ class IC3:
                   + self.formula.smtlib()
         self.solver.stdin.write(bytes(smt_input, 'utf-8'))
         if self.formula.check_sat(self.solver):
+            self.formula.get_model(self.solver)
             exit(0)
 
     def init_tr_check(self):
@@ -64,7 +65,8 @@ class IC3:
                   + self.formula.smtlib(1)
         self.solver.stdin.write(bytes(smt_input, 'utf-8'))
         if self.formula.check_sat(self.solver):
-          exit(0)
+            self.formula.get_model(self.solver, k=1)
+            exit(0)
         
     def inductive_invariant_check(self):
         self.solver.stdin.write(bytes("(reset)\n", 'utf-8'))
@@ -75,6 +77,7 @@ class IC3:
         smt_input += self.pn.smtlib_transitions_ordered(0) \
                    + self.formula.smtlib(1)
         self.solver.stdin.write(bytes(smt_input, 'utf-8'))
+        return self.formula.check_sat(self.solver)
 
     def state_reachable(self, cube):
         self.solver.stdin.write(bytes("(reset)\n", 'utf-8'))
@@ -122,9 +125,9 @@ class IC3:
 
         # Check that P is an inductive invariant
         print("> P and T => P'")
-        self.inductive_invariant_check()
-        if not self.formula.check_sat(self.solver):
+        if self.inductive_invariant_check():
             print("P is an inductive invariant! We won the war...")
+            exit(0)
         
         # while self.formula.check_sat(self.solver):
         cube = self.formula.get_model(self.solver, 0)
