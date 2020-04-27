ad545aa71db6f9b6f9ad45c791991413370b2701
Nicolas Amat <nicolas.amat@grenoble-inp.org>
2020-04-27 17:02:20 +0200

    Stepper: done

--- stepper.py
+++ stepper.py
@@ -14,7 +14,7 @@ class Stepper:
     """
     Stepper.
     """
-    def __init__(self, pn):
+    def __init__(self, pn, c=[]):
         """ Initializer.
         """
         self.pn = pn
@@ -25,6 +25,8 @@ class Stepper:
         self.pre = {}
         # A delta vector assiciated to each transition
         self.delta = {}
+
+        self.c = c
 
         self.initialization()
 
@@ -74,8 +76,9 @@ class Stepper:
 
             self.pre[tr_id], self.delta[tr_id] = pre, delta
 
-    def get_markings(self, m=None, c=None):
-        """
+    def get_markings(self, marking_vector):
+        """ Return a list of new markings not included in `c`
+            reachable in one step.
         """
         new_markings = []
 
@@ -84,26 +87,77 @@ class Stepper:
 
         # Iterate over transitions until one is fireable
         for tr in self.transitions:
-            if self.is_fireable(tr, m): # fireable
-                if True: # new marking
-                    break
+        
+            # Check if tr is fireable
+            if self.is_fireable(tr, marking_vector):
+                
+                # Compute the resulting marking
+                new_marking = [m + delta for m, delta in zip(marking_vector, self.delta[tr])]
+                marked_place = [self.pn.ordered_places[index] for index, marking in enumerate(new_marking) if marking > 0]
 
+                # Check if the new marking is not included in markings already explored
+                if self.contains_new_pair(marked_place):
+                    new_markings.append(new_marking)
+                    self.c.append(new_marking)
+        
         return new_markings
 
-    def is_fireable(self, tr, marking):
+    def is_fireable(self, tr, m):
         """ Check if a transitions is fireable given a marking.
         """
-        pre = self.pre[tr]
+        for pl_cond, pl_m in zip(self.pre[tr], m):
+            
+            # Normal Arc
+            if pl_cond > 0 and pl_m < pl_cond:
+                return False
+
+            # Inhibitor Arc
+            if pl_cond < 0 and pl_m >= pl_cond:
+                return False
+            
+        return True
+
+    def contains_new_pair(self, new_m):
+        """ Check if a new marking is not a included in a list of markings.
+        """
+        for m in self.c:
+
+            if set(new_m) <= set(m):
+                return False
+
         return True
 
 if __name__ == '__main__':
     
     if len(sys.argv) == 1:
-        exit("File missing: ./stepper <path_to_file>")
+        exit("File missing: ./stepper <path_to_net>")
     
     pn = PetriNet(sys.argv[1])
-    stepper = Stepper(pn)
+    
+    c = [[pl for pl in pn.places.values() if pl.marking > 0]]
+
+    stepper = Stepper(pn, c)
 
     print("Stepper")
     print("-------")
     print(stepper)
+
+    print("Marking Vectors")
+    print("--------")
+    
+    marking_vector = [pl.marking for pl in pn.ordered_places]
+    print("---Initial Markings---")
+    print(marking_vector)
+    
+    print("---One-step Markings---")
+    markings_1 = stepper.get_markings(marking_vector)
+    for marking in markings_1:
+        print(marking)
+
+    print("---Two-steps Markings---")
+    markings_2 = []
+    for marking in markings_1:
+        markings_2 += stepper.get_markings(marking)
+    for marking in markings_2:
+        print(marking)
+
