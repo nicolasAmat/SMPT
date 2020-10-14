9128a7eaaab6aec162dd5b717757b1086f376220
Nicolas Amat <nicolas.amat@grenoble-inp.org>
2020-10-15 00:35:16 +0200

    fast_check.sh: add verbose

--- benchmark/fast_check.sh
+++ benchmark/fast_check.sh
@@ -6,9 +6,16 @@
 # Get list of instances
 LIST=$1
 
+echo "Run reductions"
 ./run_reductions.sh $1
+
+echo "Run analysis"
 ./run_analysis.sh $1
+
+echo "Convert .out files to .csv"
 ./out2csv.py "OUTPUTS/" "oracle/" --merge-files
+
+echo "Start Jupyter notebook"
 jupyter notebook experimental_results.ipynb
 
 # Exit
