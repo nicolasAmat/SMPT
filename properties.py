f605b799f0786aebd9d9bbbd0ca8ccd26a7b1805
Nicolas Amat <namat@laas.fr>
2021-04-11 23:48:36 +0200

    properties module v3

--- properties.py
+++ properties.py
@@ -22,16 +22,27 @@ along with SMPT. If not, see <https://www.gnu.org/licenses/>.
 __author__ = "Nicolas AMAT, LAAS-CNRS"
 __contact__ = "namat@laas.fr"
 __license__ = "GPLv3"
-__version__ = "2.0.0"
+__version__ = "3.0.0"
 
 import itertools
+import operator
 import sys
 import uuid
 import xml.etree.ElementTree as ET
+from abc import ABC, abstractmethod
 
 from ptnet import PetriNet
 
-NEGATION_OPERATORS = {
+COMPARISON_OPERATORS = {
+    '=': operator.eq,
+    '<=': operator.le,
+    '>=': operator.ge,
+    '<': operator.lt,
+    '>': operator.gt,
+    'distinct': operator.ne
+}
+
+NEGATION_COMPARISON_OPERATORS = {
     '=': 'distinct',
     '<=': '>',
     '>=': '<',
@@ -40,7 +51,27 @@ NEGATION_OPERATORS = {
     'distinct': '='
 }
 
-MINIZINC_OPERATORS = {
+COMMUTATION_COMPARISON_OPERATORS = {
+    '=': '=',
+    '<=': '>=',
+    '>=': '<=',
+    '<': '>',
+    '>': '<',
+    'distinct': 'distinct'
+}
+
+PNML_BOOLEAN_OPERATORS = {
+    'negation': 'not',
+    'conjunction': 'and',
+    'disjunction': 'or'
+}
+
+NEGATION_BOOLEAN_OPERATORS = {
+    'and': 'or',
+    'or': 'and'
+}
+
+MINIZINC_BOOLEAN_OPERATORS = {
     'and': '/\\',
     'or': '\\/'
 }
@@ -124,7 +155,8 @@ class Formula:
     Formula defined by:
     - R: feared events,
     - P: invariant property,
-    - a property definition (exists-paths finally, all-paths globally).
+    - a property definition (exists-paths finally, all-paths globally),
+    - a monotonicity flag.
     """
 
     def __init__(self, ptnet, formula_xml=None):
@@ -172,7 +204,11 @@ class Formula:
             return StateFormula(operands, node)
 
         if node == 'is-fireable':
-            transitions, clauses = [self.ptnet.transitions[tr.text.replace('#', '.')] for tr in formula_xml], []
+            clauses = []
+            if self.ptnet.ids_mapping:
+                transitions = [self.ptnet.transitions[self.ptnet.transitions_mapping[tr.text.replace('#', '.')]] for tr in formula_xml]
+            else:
+                transitions = [self.ptnet.transitions[tr.text.replace('#', '.')] for tr in formula_xml]
             for tr in transitions:
                 inequalities = []
                 for pl, weight in tr.inputs.items():
@@ -205,7 +241,10 @@ class Formula:
             return Atom(left_operand, right_operand, '<=')
 
         if node == 'tokens-count':
-            places = [self.ptnet.places[place.text.replace('#', '.')] for place in formula_xml]
+            if self.ptnet.ids_mapping:
+                places = [self.ptnet.places[self.ptnet.places_mapping[place.text.replace('#', '.')]] for place in formula_xml]
+            else:
+                places = [self.ptnet.places[place.text.replace('#', '.')] for place in formula_xml]
             return TokenCount(places)
 
         if node == 'integer-constant':
@@ -246,7 +285,10 @@ class Formula:
                     ineq_R = Atom(TokenCount([pl]), IntegerConstant(-weight), '>=')
                 inequalities_R.append(ineq_R)
 
-            clauses_R.append(StateFormula(inequalities_R, 'or'))
+            if len(inequalities_R) > 1:
+                clauses_R.append(StateFormula(inequalities_R, 'or'))
+            else:
+                clauses_R.append(inequalities_R[0])
 
         self.R = StateFormula(clauses_R, 'and')
         self.P = StateFormula([self.R], 'not')
@@ -290,35 +332,40 @@ class Formula:
         self.P = StateFormula([self.R], 'not')
         self.property_def = 'finally'
 
-    def result(self, sat):
-        """ Display the result.
+    def result(self, reachable):
+        """ Display the result according to the reachability of the feared events R.
         """
         if self.property_def == 'finally':
-            if sat:
+            if reachable:
                 return "TRUE"
             else:
                 return "FALSE"
 
         if self.property_def == 'globally':
-            if sat:
+            if reachable:
                 return "FALSE"
             else:
                 return "TRUE"
 
     def dnf(self):
         """ Convert to Disjunctive Normal Form.
-            (debugging function)
         """
         formula = Formula(self.ptnet)
+        formula.non_monotonic, formula.property_def = self.non_monotonic, self.property_def
         formula.P, formula.R = self.P.dnf(), self.R.dnf()
         return formula
 
 
-class StateFormula:
+class Expression(ABC):
+    """ TODO
+    """
+
+
+class StateFormula(Expression):
     """
     StateFormula defined by:
     - a list of operands,
-    - a boolean operator (and, or, not).
+    - a boolean operator (not, and, or).
     """
 
     def __init__(self, operands, operator):
@@ -328,12 +375,8 @@ class StateFormula:
 
         if operator in ['not', 'and', 'or']:
             self.operator = operator
-        elif operator == 'negation':
-            self.operator = 'not'
-        elif operator == 'conjunction':
-            self.operator = 'and'
-        elif operator == 'disjunction':
-            self.operator = 'or'
+        elif operator in ['negation', 'conjunction', 'disjunction']:
+            self.operator = PNML_BOOLEAN_OPERATORS[operator]
         else:
             raise ValueError("Invalid operator for a state formula")
 
@@ -350,6 +393,19 @@ class StateFormula:
             text = "({})".format(text)
 
         return text
+
+    def __eq__(self, other):
+        """ Compare StateFormulas for equality.
+        """
+        if not isinstance(other, StateFormula):
+            return NotImplemented
+        else:
+            return self.operands == other.operands and self.operator == other.operator
+
+    def __hash__(self):
+        """ Hash StateFormula.
+        """
+        return hash((tuple(self.operands), self.operator))
 
     def smtlib(self, k=None, assertion=False, negation=False):
         """ State formula.
@@ -373,7 +429,7 @@ class StateFormula:
             MiniZinc format
         """
         if len(self.operands) > 1:
-            operator = MINIZINC_OPERATORS[self.operator]
+            operator = MINIZINC_BOOLEAN_OPERATORS[self.operator]
         else:
             operator = ''
 
@@ -404,6 +460,11 @@ class StateFormula:
 
         print("# Model:", model, sep='')
 
+    def negation(self):
+        """ Return the negation of the StateFormula.
+        """
+        return StateFormula([operand.negation() for operand in self.operands], NEGATION_BOOLEAN_OPERATORS[self.operator])
+
     def dnf(self, negation_propagation=False):
         """ Convert to Disjunctive Normal Form.
         """
@@ -420,23 +481,8 @@ class StateFormula:
                 # DNF(not (P and Q)) <-> DNF((not P) or (not Q))
                 return StateFormula([operand.dnf(negation_propagation) for operand in self.operands], 'or').dnf()
             else:
-                # DNF(P and Q) <-> DNF(P) and DNF(Q)
-                operands = []
-                for operand in self.operands:
-                    operand_dnf = operand.dnf()
-                    if isinstance(operand_dnf, StateFormula):
-                        operands += operand_dnf.operands
-                    else:
-                        operands.append(operand_dnf)
-                return StateFormula(operands, 'and')
-        
-        elif self.operator == 'or':
-            if negation_propagation:
-                # DNF(not (P or Q)) <-> DNF((not P) and (not Q))
-                return StateFormula([operand.dnf(negation_propagation) for operand in self.operands], 'and').dnf()
-            else:
-                # DNF(P or Q) <-> (P1 or Q1) and ... and (Pm or Q1) and ... and (Pm or Qn)
-                # with (DNF P) = (P1 and ... and Pm) and (DNF Q) = (Q1 and ... and Qn)
+                # DNF(P and Q) <-> (P1 and Q1) or ... or (Pm and Q1) or ... or (Pm and Qn)
+                # with (DNF P) = (P1 or ... or Pm) and (DNF Q) = (Q1 or ... or Qn)
                 operands = []
                 for operand in self.operands:
                     operand_dnf = operand.dnf()
@@ -447,14 +493,73 @@ class StateFormula:
 
                 clauses = []
                 for combination in itertools.product(*operands):
-                    clauses.append(StateFormula(list(combination), 'or'))
+                    combination_factorized = []
+                    for cube in combination:
+                        if isinstance(cube, StateFormula) and cube.operator ==  'and':
+                            combination_factorized += cube.operands
+                        else:
+                            combination_factorized.append(cube)
+                    clauses.append(StateFormula(combination_factorized, 'and'))
 
-            return StateFormula(clauses, 'and')
+            return StateFormula(clauses, 'or')
+        
+        elif self.operator == 'or':
+            if negation_propagation:
+                # DNF(not (P or Q)) <-> DNF((not P) and (not Q))
+                return StateFormula([operand.dnf(negation_propagation) for operand in self.operands], 'and').dnf()
+            else:
+                # DNF(P and Q) <-> DNF(P) and DNF(Q)
+                operands_dnf = []
+                
+                for operand in self.operands:
+                    operand_dnf = operand.dnf()
+                    if isinstance(operand_dnf, StateFormula):
+                        operands_dnf += operand_dnf.operands
+                    else:
+                        operands_dnf.append(operand_dnf)
+                return StateFormula(operands_dnf, 'or')
 
         else:
             raise ValueError("Invalid operator for a state formula")
 
-class Atom:
+    def reached_cube(self, m):
+        """ Return a cube satisfied by marking m.
+            Assume the formula is in DNF and satisfied by m.
+        """
+        if self.operator == 'or':
+            for cube in self.operands:
+                if cube.eval(m):
+                    return cube
+        else:
+            return self
+
+    def eval(self, m):
+        """ Evaluate the StateFomula with marking m.
+        """
+        if self.operator == 'not':
+            return not self.operands[0].eval(m)
+        
+        elif self.operator == 'and':
+            return all(operand.eval(m) for operand in self.operands)
+        
+        elif self.operator == 'or':
+            return any(operand.eval(m) for operand in self.operands)
+        
+        else:
+            return False
+
+    def generalization(self, tr):
+        """ Generalize a state leading to feared states.
+        """
+        generalized_operands = [operand.generalization(tr, firing_condition=False) for operand in self.operands]
+
+        for place, weight in tr.pre.items():
+            generalized_operands.append(Atom(TokenCount([place]), IntegerConstant(weight), '>='))    
+
+        return StateFormula(generalized_operands, 'and')
+
+
+class Atom(Expression):
     """
     Atom defined by:
     - a left operand,
@@ -478,6 +583,19 @@ class Atom:
             (debugging function)
         """
         return "({} {} {})".format(self.left_operand, self.operator, self.right_operand)
+
+    def __eq__(self, other):
+        """ Compare Atoms for equality.
+        """
+        if not isinstance(other, Atom):
+            return NotImplemented
+        else:
+            return self.left_operand == other.left_operand and self.right_operand == other.right_operand and self.operator == other.operator
+
+    def __hash__(self):
+        """ Hash Atom.
+        """
+        return hash((self.left_operand, self.operator, self.right_operand))
 
     def smtlib(self, k=None, assertion=False, negation=False):
         """ Atom.
@@ -504,27 +622,66 @@ class Atom:
 
         return minizinc_input
 
+    def negation(self):
+        """ Return the negation of the Atom.
+        """
+        return Atom(self.left_operand, self.right_operand, NEGATION_COMPARISON_OPERATORS[self.operator])
+
     def dnf(self, negation_propagation=False):
         """ Convert to Disjunctive Normal Form.
         """
         if negation_propagation:
             # DNF(not (P comp Q)) <-> P (not comp) Q
-            return Atom(self.left_operand, self.right_operand, NEGATION_OPERATORS[self.operator])
+            return Atom(self.left_operand, self.right_operand, NEGATION_COMPARISON_OPERATORS[self.operator]).dnf()
         else:
             # DNF(P comp Q) <-> P comp Q 
-            return self
+            if isinstance(self.left_operand, IntegerConstant) and isinstance(self.right_operand, TokenCount):
+                # Normalization: TokenCount at left and IntegerConstant at right
+                return Atom(self.right_operand, self.left_operand, COMMUTATION_COMPARISON_OPERATORS[self.operator])
+            else:
+                return self
+
+    def reached_cube(self, m):
+        """ Return a cube satisfied by marking m.
+            Assume the formula is in DNF and satisfied by m.
+        """
+        return self
+
+    def eval(self, m):
+        """ Evaluate the subformula with marking m.
+        """
+        return COMPARISON_OPERATORS[self.operator](self.left_operand.eval(m), self.right_operand.eval(m))
+
+    def generalization(self, tr, firing_condition=True):
+        """ Generalize a state leading to feared states.
+        """
+        left_operand = self.left_operand.generalization(tr)
+        right_operand = self.right_operand.generalization(tr)
+
+        # Normalization: value at right
+        right_operand.value -= left_operand.value
+        left_operand.value = 0
+
+        if firing_condition:
+            atoms = [Atom(left_operand, right_operand, self.operator)]
+            for place, weight in tr.pre.items():
+                atoms.append(Atom(TokenCount([place]), IntegerConstant(weight), '>='))
+            return StateFormula(atoms, 'and')
+        else:
+            return Atom(left_operand, right_operand, self.operator)
 
 
-class TokenCount:
+class TokenCount(Expression):
     """
     Token count defined by:
     - a list of places.
     """
 
-    def __init__(self, places):
+    def __init__(self, places, value=0):
         """ Initializer
         """
         self.places = places
+        self.value = value
 
     def __str__(self):
         """ Token count to textual format.
@@ -532,10 +689,26 @@ class TokenCount:
         """
         smt_input = ' + '.join(map(lambda pl: pl.id, self.places))
 
+        if self.value:
+            smt_input += " {} {}".format(self.sign(), abs(self.value))
+
         if len(self.places) > 1:
             smt_input = "({})".format(smt_input)
 
         return smt_input
+
+    def __eq__(self, other):
+        """ Compare TokenCounts for equality.
+        """
+        if not isinstance(other, TokenCount):
+            return NotImplemented
+        else:
+            return self.places == other.places and self.value == other.value
+
+    def __hash__(self):
+        """ Hash TokenCount.
+        """
+        return hash((tuple(self.places), self.value))
 
     def smtlib(self, k=None):
         """ Token count.
@@ -549,6 +722,9 @@ class TokenCount:
         if len(self.places) > 1:
             smt_input = "(+ {})".format(smt_input)
 
+        if self.value:
+            smt_input = "({} {} {})".format(self.sign(), smt_input, abs(self.value))
+
         return smt_input
 
     def minizinc(self):
@@ -560,16 +736,44 @@ class TokenCount:
         if len(self.places) > 1:
             minizinc_input = "({})".format(minizinc_input)
 
+        if self.value:
+            minizinc_input = "({} {} {})".format(minizinc_input, self.sign(), self.value)
+
         return minizinc_input
 
     def dnf(self, negation_propagation=False):
         """ Convert to Disjunctive Normal Form.
         """
+        # Normalization: lexicographic order
+        self.places = sorted(self.places, key=lambda pl: pl.id)
+
         # DNF(P1 + ... + Pn) = P1 + ... + Pn
         return self
 
+    def sign(self):
+        """ Return the sign of the value.
+        """
+        if self.value < 0:
+            return '-'
+        else:
+            return '+'
 
-class IntegerConstant:
+    def eval(self, m):
+        """ Evaluate the subformula with marking m.
+        """
+        return sum([m[pl] for pl in self.places]) + self.value
+
+    def generalization(self, tr):
+        """ Generalize a state leading to feared states.
+        """
+        pre = sum([tr.inputs[place] for place in self.places if place in tr.inputs])
+        post = sum([tr.outputs[place] for place in self.places if place in tr.outputs])
+        
+        delta = post - pre
+        
+        return TokenCount(self.places, self.value + delta)
+
+class IntegerConstant(Expression):
     """ 
     Integer constant.
     """
@@ -584,6 +788,19 @@ class IntegerConstant:
             (debugging function)
         """
         return str(self.value)
+
+    def __eq__(self, other):
+        """ Compare IntegerConstants for equality.
+        """
+        if not isinstance(other, IntegerConstant):
+            return NotImplemented
+        else:
+            return self.value == other.value
+
+    def __hash__(self):
+        """ Hash IntegerConstant.
+        """
+        return hash(self.value)
 
     def smtlib(self, k=None):
         """ Integer constant.
@@ -602,6 +819,16 @@ class IntegerConstant:
         """
         # DNF(k) = k
         return self
+
+    def eval(self, m):
+        """ Evaluate the subformula with marking m.
+        """
+        return self.value
+
+    def generalization(self, tr):
+        """ Generalize a state leading to feared states.
+        """
+        return IntegerConstant(self.value)
 
 
 if __name__ == '__main__':
