4e0b7d4950ff069873021a89fc4d28aed538266e
Nicolas Amat <namat@laas.fr>
2021-08-09 14:56:34 +0200

    Update 2pnml script

--- benchmark/2pnml.py
+++ benchmark/2pnml.py
@@ -14,6 +14,8 @@ from pathlib import Path
 def translator(filename):
     """ Translator.
     """
+    mapping = {}
+
     # Convert to `.pnml`
     output = subprocess.run(["ndrio", filename, '-pnml'], stdout=subprocess.PIPE).stdout.decode('utf-8')
     
@@ -27,12 +29,19 @@ def translator(filename):
     # Set place `id`s to `text/name`
     for place_node in root.iter(xmlns + 'place'):
         place_name = place_node.find(xmlns + 'name/' + xmlns + 'text')
+        mapping[place_node.attrib['id']] = place_name.text
         place_node.attrib['id'] = place_name.text
         
     # Set transition `id`s to `text/name`
     for transition_node in root.iter(xmlns + 'transition'):
         transition_name = transition_node.find(xmlns + 'name/' + xmlns + 'text')
+        mapping[transition_node.attrib['id']] = transition_name.text
         transition_node.attrib['id'] = transition_name.text
+
+    # Set arcs source and target coherent
+    for arc_node in root.iter(xmlns + 'arc'):
+        arc_node.attrib['source'] = mapping[arc_node.attrib['source']]
+        arc_node.attrib['target'] = mapping[arc_node.attrib['target']] 
 
     # Get output filename
     filename = Path(filename)
