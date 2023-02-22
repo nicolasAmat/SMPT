e8a12089c1585632ad3f25be650709e21892b84d
Nicolas Amat <namat@laas.fr>
2023-02-23 00:31:17 +0100

    Update `bulk.py` meta method

--- smpt/checkers/bulk.py
+++ smpt/checkers/bulk.py
@@ -1,5 +1,6 @@
 """
 Bulk Meta Method (PDR-REACH-SATURATED (10sec and optional) -> COMPOUND -> WALK)
+This method is specific to the Model Checking Contest.
 
 This file is part of SMPT.
 
@@ -19,7 +20,6 @@ along with SMPT. If not, see <https://www.gnu.org/licenses/>.
 
 from __future__ import annotations
 
-
 __author__ = "Nicolas AMAT, LAAS-CNRS"
 __contact__ = "namat@laas.fr"
 __license__ = "GPLv3"
@@ -29,10 +29,10 @@ from logging import info
 from multiprocessing import Process, Queue
 from typing import Optional
 
-from smpt.checkers.randomwalk import RandomWalk
 from smpt.checkers.abstractchecker import AbstractChecker
 from smpt.checkers.compound import Compound
 from smpt.checkers.pdr import PDR
+from smpt.checkers.randomwalk import RandomWalk
 from smpt.exec.utils import KILL, STOP, send_signal_group_pid, send_signal_pids
 from smpt.interfaces.walk import Walk
 from smpt.ptio.formula import Formula, Properties
@@ -44,84 +44,66 @@ class Bulk(AbstractChecker):
     Bulk meta method.
     """
 
-    def __init__(self, ptnet: PetriNet, formula: Formula, properties: Properties, pdr: bool = False, debug: bool = False, solver_pids: Optional[Queue[int]] = None, additional_techniques: Optional[Queue[str]] = None):
+    def __init__(self, ptnet: PetriNet, formula: Formula, properties: Properties, formula_compound: Formula, pdr: bool = False, debug: bool = False, solver_pids: Optional[Queue[int]] = None, bulk_techniques: Optional[Queue[str]] = None):
         """ Initializer.
         """
-        # Petri net
-        self.ptnet = ptnet
-
-        # Formula
-        self.formula = formula
-
-        # Properties context
-        self.properties = properties
+        # Local queue of solver pids (for PDR)
+        self.solver_pids_bis: Queue[int] = Queue() if pdr else None
 
         # Additional techniques
-        self.additional_techniques = additional_techniques
-
-        # Local queue of solver pids (for PDR)
-        self.solver_pids_bis: Queue[int] = Queue()
+        self.bulk_techniques = bulk_techniques
 
         # Methods
         self.pdr = PDR(ptnet, formula, debug=debug, method='REACH', saturation=True, solver_pids=solver_pids, solver_pids_bis=self.solver_pids_bis) if pdr else None
-        self.compound = Compound(formula, properties, debug=debug, solver_pids=solver_pids, solver_pids_bis=self.solver_pids_bis)
-        self.walk = RandomWalk(ptnet, formula, debug=debug, solver_pids=solver_pids, solver_pids_bis=self.solver_pids_bis)
+        self.compound = Compound(formula_compound, properties, debug=debug, solver_pids=solver_pids)
+        self.walk = RandomWalk(ptnet, formula, debug=debug, solver_pids=solver_pids)
 
     def prove(self, result, concurrent_pids):
         """ Prover.
         """
         info("[BULK] RUNNING")
 
-        found_verdict = False
-
+        # Run PDR (if enabled)
         if self.pdr:
-            found_verdict = self.run_helper(self.pdr.prove, 10, ['IMPLICIT', 'SAT-SMT', 'PDR_REACH_SATURATED'], result, concurrent_pids)
+            self.run_helper(self.pdr.prove, 10, ['IMPLICIT', 'SAT-SMT', 'PDR_REACH_SATURATED'], result, concurrent_pids)
             self.pdr = None
 
-        if not found_verdict:
-            found_verdict = self.run_helper(self.compound.prove, None, ['COMPOUND'], result, concurrent_pids)
-            self.compound = None
+        # Run COMPOUND
+        self.run_helper(self.compound.prove, None, ['COMPOUND'], result, concurrent_pids)
+        self.compound = None
 
-        if not found_verdict:
-            self.run_helper(self.walk.prove, None, ['WALK'], result, concurrent_pids)
-            self.walk = None
+        # Run WALK (no Parikh, no slicing)
+        self.run_helper(self.walk.prove, None, ['WALK'], result, concurrent_pids)
+        self.walk = None
 
     def run_helper(self, prover, timeout, techniques, result, concurrent_pids):
         """ Run and manage methods.
         """
-        found_verdict = False
+        # Remove all bulk techniques
+        while not self.bulk_techniques.empty():
+            self.bulk_techniques.get()
 
-        local_result = Queue()
-        local_concurrent_pids = Queue()
+        # Add new techniques
+        for technique in techniques:
+            self.bulk_techniques.put(technique)
+
+        # Get pids of concurrent methods
+        pids = concurrent_pids.get()
 
         # Run process
-        proc = Process(target=prover, args=(local_result, local_concurrent_pids,))
+        proc = Process(target=prover, args=(result, concurrent_pids,))
         proc.start()
         proc_id = proc.pid
+
+        # Return pids (by adding the new process)
+        concurrent_pids.put(pids + [proc_id])
+
         proc.join(timeout=timeout)
-
-        # Check if there is some result
-        if not local_result.empty():
-
-            # Found a verdict
-            found_verdict = True
-            
-            # Add additional techniques
-            for technique in techniques:
-                self.additional_techniques.put(technique)
-        
-            # Transfer the result
-            result.put(local_result.get())
-
-            # Terminate concurrent methods
-            if not concurrent_pids.empty():
-                send_signal_pids(concurrent_pids.get(), STOP)
 
         # Kill method
         send_signal_pids([proc_id], KILL)
 
         # Kill local solvers
-        while not self.solver_pids_bis.empty():
-            send_signal_group_pid(self.solver_pids_bis.get(), KILL)
-
-        return found_verdict
+        if self.solver_pids_bis:
+            while not self.solver_pids_bis.empty():
+                send_signal_group_pid(self.solver_pids_bis.get(), KILL)
